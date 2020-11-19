0ef78e469c954d3604c05ecb228ab9bfe7d1c952
Nathin <nwascher1@gmail.com>
2020-11-19 12:40:53 -0600

    added pop-up box

--- javacode/PokemonObject.java
+++ javacode/PokemonObject.java
@@ -5,12 +5,17 @@
  * This file is under the MIT License.
  */
 
+import utility.Misc;
+import utility.PopUpBox;
+
 import java.util.ArrayList;
 
 /**
  * @author Nathin Wascher
+ * @version v1.1 - November 19, 2020
  */
 public class PokemonObject {
+    private ArrayList<String> pokedexEntry;
     private String pokemon;
 
     public PokemonObject() {
@@ -19,39 +24,54 @@ public class PokemonObject {
     /**
      * Prints a pokemon's information.
      * 
+     * @param pokedex
      * @param pokedexPos The position of the {@code JSON} name "<i>name</i>" in the
      *                   {@code pokedex ArrayList}.
      */
     public void processInfo(ArrayList<String> pokedex, int pokedexPos) {
-        pokedexPos++;
         String[] tempArray;
-        String region = foundInRegion(pokedex.get(pokedexPos + 2));
         int pokeNum;
 
+        pokedexEntry = new ArrayList<String>();
+        for (int g = 0; g < 8; g++)
+            pokedexEntry.add(pokedex.get(pokedexPos + g));
+
+        String region = foundInRegion(pokedexEntry.get(3));
+
         // Adds the region and pokemon evolution
-        pokemon = region + "  Evo:" + pokedex.get(pokedexPos + 6);
+        pokemon = region + "  Evo:" + pokedexEntry.get(7);
 
         // Adds the pokemon number
-        pokeNum = Integer.parseInt(pokedex.get(pokedexPos + 2));
-        if (pokeNum > 999)
+        pokeNum = Integer.parseInt(pokedexEntry.get(3));
+        if (pokeNum >= 1000)
             pokemon = pokemon + "  #" + pokeNum;
-        else if (pokeNum > 99)
+        else if (pokeNum >= 100)
             pokemon = pokemon + "  #0" + pokeNum;
-        else if (pokeNum > 9)
+        else if (pokeNum >= 10)
             pokemon = pokemon + "  #00" + pokeNum;
         else
             pokemon = pokemon + "  #000" + pokeNum;
 
         // Adds the pokemon's name
-        pokemon = pokemon + "  " + pokedex.get(pokedexPos + 0) + "  ";
-        for (int i = pokedex.get(pokedexPos + 0).length(); i < 12; i++)
+        pokemon = pokemon + "  " + pokedexEntry.get(1) + "  ";
+        for (int i = pokedexEntry.get(1).length(); i < 12; i++)
             pokemon = pokemon + " ";
 
         // Adds the types
-        tempArray = pokedex.get(pokedexPos + 4).split("-");
+        tempArray = pokedexEntry.get(5).split("-");
         pokemon = pokemon + tempArray[0];
         if (tempArray.length == 2)
             pokemon = pokemon + ", " + tempArray[1];
+    }
+
+    public void showInfoBox() {
+        ArrayList<String> parse = new ArrayList<>();
+
+        for (int i = 0; i < pokedexEntry.size(); i++)
+            parse.add(Misc.capitalize(pokedexEntry.get(i)) + " : " + pokedexEntry.get(++i));
+
+        PopUpBox tempPUB = new PopUpBox(pokedexEntry.get(1), parse);
+        tempPUB.createBox();
     }
 
     /**
@@ -82,7 +102,7 @@ public class PokemonObject {
         else if (pokeNum <= 898)
             return "Galar ";
         else
-            return "NONE???";
+            return "NULL";
     }
 
     @Override
